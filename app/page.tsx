7da60b22d1751ff9d9d13edfd7acecf9c32d8c35
Moein-Kazemi <moeinkazemi.dev@gmail.com>
2026-07-11 23:26:18 +0330

    1405/04/19 : add custom banner for consept

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Card3d from "@/components/Card3d";
 import { vazirMedium } from "./fonts";
 import Link from "next/link";
 
@@ -21,24 +22,7 @@ export default function Home() {
               شروع
             </Link>
 
-            <div className="hover-3d">
-              {/* content */}
-              <figure className="max-w-100 rounded-2xl">
-                <img
-                  src="https://img.daisyui.com/images/stock/creditcard.webp"
-                  alt="3D card"
-                />
-              </figure>
-              {/* 8 empty divs needed for the 3D effect */}
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
+            <Card3d src="/images/consept.png" />
           </div>
         </div>
       </section>
