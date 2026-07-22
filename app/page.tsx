3134f405a4b504951b922a356aa99ec5f9c15c98
Moein-Kazemi <moeinkazemi.dev@gmail.com>
2026-07-22 22:51:40 +0330

    1405/04/31 : complete banner and title section in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,52 @@
-import Card3d from "@/components/Card3d";
 import { vazirMedium } from "./fonts";
 import Link from "next/link";
+import { Check } from "lucide-react";
+import Card3d from "@/components/Card3d";
 
 export default function Home() {
   return (
     <main>
       <section className="hero min-h-[60vh] bg-base-100">
-        <div className="hero-content text-center">
-          <div className="max-w-2xl">
-            <h1
-              className={`text-3xl ${vazirMedium.className} md:text-5xl  mb-6`}
-            >
-              تیم بست | مدیریت تیم خود را متحول کنید.
+        <div className="hero-content text-center grid grid-cols-12">
+          <Card3d src="/images/consept.png" />
+          <div className="max-w-2xl space-y-8  col-span-12 sm:col-span-4">
+            <h1 className={`text-3xl ${vazirMedium.className} md:text-5xl`}>
+              مدیریت تیم،
+              <span className="text-primary block mt-4">بدون پیچیدگی.</span>
             </h1>
-            <p className="text-lg mb-8">
-              پلتفرمی یکپارچه برای تیم‌های حرفه‌ای. از تشکیل تیم تا گزارش‌گیری،
-              همه چیز در دستان شماست.
+            <p className="text-lg  text-right">
+              مدیریت تیم ها ، وظایف و پروژه ها در یک فضای هوشمند آنلاین و حرفه
+              ای.
             </p>
-            <p className="text-lg mb-8">طراحی و توسعه معین کاظمی</p>
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-1 text-right">
+                <span className="w-6 h-6 flex items-center justify-center bg-blue-500 rounded-full">
+                  <Check size={16} />
+                </span>
+                <div className="block">مدیریت پروژه</div>
+              </div>
+              <div className="flex items-center gap-1 text-right">
+                <span className="w-6 h-6 flex items-center justify-center bg-blue-500 rounded-full">
+                  <Check size={16} />
+                </span>
+                <div className="block">مدیریت وظایف و زمانبندی</div>
+              </div>
+              <div className="flex items-center gap-1 text-right">
+                <span className="w-6 h-6 flex items-center justify-center bg-blue-500 rounded-full">
+                  <Check size={16} />
+                </span>
+                <div className="block">گزارش گیری</div>
+              </div>
+              <div className="flex items-center gap-1 text-right">
+                <span className="w-6 h-6 flex items-center justify-center bg-blue-500 rounded-full">
+                  <Check size={16} />
+                </span>
+                <div className="block">همکاری تیمی</div>
+              </div>
+            </div>
             <Link href="/dashboard" className="btn btn-primary btn-lg">
               شروع
             </Link>
-
-            <Card3d src="/images/consept.png" />
           </div>
         </div>
       </section>
--- components/Card3d.tsx
+++ components/Card3d.tsx
@@ -2,14 +2,11 @@ interface Card3dProps {
   src: string;
 }
 function Card3d({ src }: Card3dProps) {
-  console.log(src);
   return (
-    <div className="hover-3d">
-      {/* content */}
-      <figure className="max-w-100 rounded-2xl">
+    <div className="hover-3d col-span-12 sm:col-span-8">
+      <figure className="max-w-200 rounded-2xl">
         <img src={`${src}`} alt="3D card" />
       </figure>
-      {/* 8 empty divs needed for the 3D effect */}
       <div></div>
       <div></div>
       <div></div>
